1244124fd26169744e19a642ed354a771ef54a53
zBl4ck <zBl4ckDev@protonmail.com>
2024-01-31 13:57:54 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
-      <Header></Header>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+            {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
     </div>
   );
 }
